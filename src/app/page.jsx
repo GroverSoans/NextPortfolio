da82c8f92f6925ec0fd7430fe50b4dd9916c7eb8
GroverSoans <groverpsoans@gmail.com>
2025-01-21 18:30:19 -0600

    pink styling

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="bg-black">
       <header className="w-full">
-        <h1 className="text-white fixed left-0 z-20 p-[30px] text-xl font-bold">by Grover Soans ™</h1>
+        <h1 className="text-white fixed left-0 z-20 p-[30px] text-xl font-bold">by Grover Soans<span className="text-pink-500">™</span></h1>
         <Clock className="flex justify-center"/>
       </header>
       <InfiniteTextScroll />
