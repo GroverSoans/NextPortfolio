6bd778620aef357cbdc2b732d1827e8a760988d6
Grover Soans <groverpsoans@gmail.com>
2025-01-25 17:14:12 -0600

    updted project card import

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import ProjectCard from "@/components/ProjectCard"
+import ProjectCard from "@/components/projectCard"
 import { useNavStore } from '@/store/navStore';
 import { projects } from '@/constants/projects';
 import PageTransition from '@/components/pageTransition';
